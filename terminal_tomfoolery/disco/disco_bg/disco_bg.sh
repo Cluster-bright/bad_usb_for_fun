b87ca622606c857aafc8ee3797c1280151bafe06
Sohann Perrin <sohann1512@gmail.com>
2024-09-30 13:35:00 +0200

    disco

---
+++ terminal_tomfoolery/disco/disco_bg/disco_bg.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# set -m
+
+echo $$ >> ~/.tcrc/backupgradpid.txt
+pidof -n bash > tmptrupidlist.txt
+trupid=$(cut --fields=1 -d ' ' tmptrupidlist.txt)
+rm tmptrupidlist.txt
+
+r=0
+b=0
+g=0
+randr=$(expr $RANDOM % 3)
+randb=$(expr $RANDOM % 3)
+randg=$(expr $RANDOM % 3 - 3)
+
+# coproc read -t $(expr $RANDOM % 50 + 10) && wait "$!" || true
+
+function increasecolors {
+    ret=$(expr $1 + $2)
+    if [ $1 -gt 255 ]
+    then
+        ret=`expr $1 % 256`
+    fi
+    if [ $1 -lt 0 ]
+    then
+        ret=`expr $1 + 256`
+    fi
+    echo $ret
+}
+
+
+while [ 1 ];
+do
+    # trap ' '2
+    r=`increasecolors $r $randr`
+    b=`increasecolors $b $randb`
+    g=`increasecolors $g $randg`
+    (echo -n $(printf '\033]11;#%x%x%x\007' "$r" "$g" "$b" ) > /proc/$trupid/fd/1) || exit
+    # (echo -n $(printf '\033]10;#%x%x%x\007' "$b" "$r" "$g" ) > /proc/$trupid/fd/1) || exit
+    coproc read -t 0.1 && wait "$!" || true
+done
