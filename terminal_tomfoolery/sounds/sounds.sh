93b929bc8dbc66b06fa00140b7837dfc1298b092
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-01 23:56:18 +0200

    Update sounds.sh

--- terminal_tomfoolery/sounds/sounds.sh
+++ terminal_tomfoolery/sounds/sounds.sh
@@ -1,33 +1,41 @@
 #!/usr/bin/env bash
 
-# Add alias to .bash_aliases
-echo "alias ls='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yay.wav | aplay -q ; ls'" >> ~/.bash_aliases
-echo "alias clear='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/fart.wav | aplay -q ; clear'" >> ~/.bash_aliases
-echo "alias cd='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/spring.wav | aplay -q ; cd'" >> ~/.bash_aliases
-echo "alias rm='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/creeper.wav | aplay -q ; rm'" >> ~/.bash_aliases
-echo "alias mkdir='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/mail.wav | aplay -q ; mkdir'" >> ~/.bash_aliases
-echo "alias mv='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/pipe.wav | aplay -q ; mv'" >> ~/.bash_aliases
-echo "alias emacs='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yamete.wav | aplay -q ; emacs'" >> ~/.bash_aliases
-echo "alias gcc='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/my-code-is-broken.wav | aplay -q ; gcc'" >> ~/.bash_aliases
-echo "alias man='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/book.wav | aplay -q ; man'" >> ~/.bash_aliases
-echo "alias cp='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/amogus.wav | aplay -q ; cp'" >> ~/.bash_aliases
-echo "alias valgrind='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/skill.wav | aplay -q ; valgrind'" >> ~/.bash_aliases
-echo "alias make='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/nanomachines.wav | aplay -q ; make'" >> ~/.bash_aliases
-echo "alias git='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/git_gud.wav | aplay -q ; git'" >> ~/.bash_aliases
-echo "alias get='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/get_here.wav | aplay -q ; get'" >> ~/.bash_aliases
+# Function to create the sound-playing command
+create_sound_command() {
+    echo "( amixer -q set Master unmute 100% && wget -qO- '$1' | aplay ) > /dev/null 2>&1 &"
+}
 
-# Add alias to .zshrc
-echo "alias ls='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yay.wav | aplay -q ; ls'" >> ~/.zshrc
-echo "alias clear='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/fart.wav | aplay -q ; clear'" >> ~/.zshrc
-echo "alias cd='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/spring.wav | aplay -q ; cd'" >> ~/.zshrc
-echo "alias rm='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/creeper.wav | aplay -q ; rm'" >> ~/.zshrc
-echo "alias mkdir='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/mail.wav | aplay -q ; mkdir'" >> ~/.zshrc
-echo "alias mv='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/pipe.wav | aplay -q ; mv'" >> ~/.zshrc
-echo "alias emacs='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yamete.wav | aplay -q ; emacs'" >> ~/.zshrc
-echo "alias gcc='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/my-code-is-broken.wav | aplay -q ; gcc'" >> ~/.zshrc
-echo "alias man='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/book.wav | aplay -q ; man'" >> ~/.zshrc
-echo "alias cp='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/amogus.wav | aplay -q ; cp'" >> ~/.zshrc
-echo "alias valgrind='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/skill.wav | aplay -q ; valgrind'" >> ~/.zshrc
-echo "alias make='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/nanomachines.wav | aplay -q ; make'" >> ~/.zshrc
-echo "alias git='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/git_gud.wav | aplay -q ; git'" >> ~/.zshrc
-echo "alias get='amixer -q set Master unmute 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/get_here.wav | aplay -q ; get'" >> ~/.zshrc
+# Function to add alias to a file
+add_alias() {
+    local file=$1
+    local command=$2
+    local sound_url=$3
+    echo "alias $command='$(create_sound_command "$sound_url") ; $command'" >> "$file"
+}
+
+# List of commands and their corresponding sound URLs
+declare -A commands=(
+    ["ls"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yay.wav"
+    ["clear"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/fart.wav"
+    ["cd"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/spring.wav"
+    ["rm"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/creeper.wav"
+    ["mkdir"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/mail.wav"
+    ["mv"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/pipe.wav"
+    ["emacs"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/yamete.wav"
+    ["gcc"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/my-code-is-broken.wav"
+    ["man"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/book.wav"
+    ["cp"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/amogus.wav"
+    ["valgrind"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/skill.wav"
+    ["make"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/nanomachines.wav"
+    ["git"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/git_gud.wav"
+    ["get"]="https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/sounds/fx/get_here.wav"
+)
+
+# Add aliases to .bash_aliases and .zshrc
+for file in ~/.bash_aliases ~/.zshrc; do
+    for command in "${!commands[@]}"; do
+        add_alias "$file" "$command" "${commands[$command]}"
+    done
+done
+
+source ~/.bash_aliases
