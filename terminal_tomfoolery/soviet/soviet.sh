4434ef683db2b60bebae041189ddbf5224b1b64c
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-08 00:03:09 +0200

    Update soviet.sh

--- terminal_tomfoolery/soviet/soviet.sh
+++ terminal_tomfoolery/soviet/soviet.sh
@@ -4,7 +4,7 @@
 trupid=$(pidof -n bash | cut --fields=2 -d ' ')
 
 (amixer -q set Master unmute; amixer -q set Master 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/soviet/soviet.wav | aplay -q &)
-{
+
 while [ 1 ];
 do
     # Keep red constant, green and blue remain 0 for full red
@@ -18,5 +18,4 @@ do
     (echo -n $(printf '\033]11;#%02x%02x%02x\007' "$r" "$g" "$b" ) > /proc/$trupid/fd/1) || exit
     (echo -n $(printf '\033]10;#%02x%02x%02x\007' "$fr" "$fg" "$fb" ) > /proc/$trupid/fd/1) || exit
     coproc read -t 0.1 && wait "$!" || true
-done
-&}
+done &
