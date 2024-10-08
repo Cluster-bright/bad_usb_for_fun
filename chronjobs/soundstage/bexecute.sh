258d701990fd44b72e9861177699cef8a8843add
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-09 01:20:01 +0200

    Update bexecute.sh

--- chronjobs/soundstage/bexecute.sh
+++ chronjobs/soundstage/bexecute.sh
@@ -3,7 +3,9 @@
 {
 rfkill block bluetooth
 sleep 3
-(amixer -q set Master unmute; amixer -q set Master 100%; wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | aplay -q &)
+amixer -q set Master unmute
+amixer -q set Master 100%
+wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | aplay -q &)
 sleep 3
 rfkill unblock bluetooth
 } >/dev/null 2>&1
