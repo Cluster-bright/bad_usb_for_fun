998d43b59827c01ef99679a58f5f9c26bf2108ef
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2024-10-15 21:09:20 +0200

    Delete chronjobs/soundstage/buninstall.sh

--- chronjobs/soundstage/buninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cronjob="*/37 * * * * ~/.bnodes/.bexecute.sh"
-
-crontab -l | grep -F "$cronjob" > /dev/null
-
-if [ $? -eq 0 ]; then
-    crontab -l | grep -v -F "$cronjob" | crontab -
